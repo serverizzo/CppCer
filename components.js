41c43554ad50c60960ca719497ec23c713ff20b3
Edward Cerverizzo <edward.cerverizzo@gmail.com>
2020-01-03 12:45:04 -0500

    added material for 8 queens. added file for additional resources. homepage improved

---
+++ components.js
@@ -0,0 +1,13 @@
+
+class Title extends React.Compnent{
+
+    render(){
+        return(
+            <h1>Hello World!</h1>
+        )
+    }
+
+}
+ReactDOM.render(<Title />, document.getElementById("Title"));
+
+export default Title;
