620575a7e1cedea76878bed76f0c1786161d79f1
Edward Cerverizzo <edward.cerverizzo@gmail.com>
2020-01-23 16:00:25 -0500

    worked on stablemarriage page and added links to number systems in notes folder (also added).

--- components.js
+++ components.js
@@ -1,13 +1,14 @@
+import React from "react";
+import ReactDOM from 'react-dom';
 
-class Title extends React.Compnent{
+
+class Title extends React.Component{
 
     render(){
         return(
-            <h1>Hello World!</h1>
+            <h1>Hello World! I am from the components</h1>
         )
     }
 
 }
 ReactDOM.render(<Title />, document.getElementById("Title"));
-
-export default Title;
