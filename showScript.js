42b8084aa0128547431a34e5258a9c7f3a3fa50c
Edward Cerverizzo <edward.cerverizzo@gmail.com>
2020-04-11 10:56:29 -0400

    added previously untracked documents to git

---
+++ showScript.js
@@ -0,0 +1,10 @@
+function show(){
+    var toggleDiv = document.getElementById("hiddenDiv")
+
+    if (toggleDiv.style.display === "none") {
+        toggleDiv.style.display = "block";
+      } 
+      else {
+        toggleDiv.style.display = "none";
+      }
+    }
